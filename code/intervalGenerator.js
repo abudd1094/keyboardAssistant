13409641bae297226bceae48a057f506ce07670c
Alec Budd <alec.budd@prodyna.com>
2024-01-21 16:02:20 +0100

    chore: documented / organized intervalGenerator.js

--- code/intervalGenerator.js
+++ code/intervalGenerator.js
@@ -1,12 +1,23 @@
+/**
+ * @file intervalGenerator.js
+ * @description Takes a single interval and replicates it up and down across all octaves.
+ */
 inlets = 1;
 setinletassist(0, "\n Interval pair in");
 outlets = 1;
 setoutletassist(0, "\n 0: Intervals out");
 
+/**
+ * @function list
+ * @description Takes a single interval and replicates it up and down across all octaves.
+ * @param {...number} intervalPair - MIDI note values representing the interval pair.
+ */
 function list() {
     var a = arrayfromargs(arguments);
+
     var root = a[0];
     var interval = a[1] - a[0];
+    
     var ascending = root + interval;
     var descending = root - interval;
 
